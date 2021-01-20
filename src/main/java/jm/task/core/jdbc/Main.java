615bb8e7b83f4fd9fc5d1f2f0ed02bd47bcb8233
fatron558 <73903211+fatron558@users.noreply.github.com>
2021-01-20 20:06:20 +0500

    Не исправен Test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,21 +2,30 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.dropUsersTable();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("Alexey", "Kireev", (byte) 26);
-        userDaoJDBC.saveUser("Alexey", "Kireev", (byte) 26);
-        List<User> users = userDaoJDBC.getAllUsers();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.dropUsersTable();
+        userService.dropUsersTable();
+        userService.dropUsersTable();
+        userService.dropUsersTable();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Alexey", "Kireev", (byte) 26);
+        userService.saveUser("Anduin", "Rinn", (byte) 30);
+        userService.removeUserById(1);
+        List<User> users = userService.getAllUsers();
         for (User user : users) {
             System.out.println(user.toString());
         }
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
